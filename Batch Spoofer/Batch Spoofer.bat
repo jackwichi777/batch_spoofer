e41149beecde8c2843308fed55b83d0c0c0f3a04
jackwichi777 <jackwichi7773@gmail.com>
2024-06-08 08:19:02 +0900

    Update Batch Spoofer.bat

--- Batch Spoofer/Batch Spoofer.bat
+++ Batch Spoofer/Batch Spoofer.bat
@@ -1,5 +1,5 @@
 @echo off
-title Batch Spoofer 1.1.0 - Developed By jackkkkk
+title jack Spoofer 1.1.0 - Developed By jackkkkk
 	cls
 	set interface_admin_state=Not Selected
 	set interface_registry_location=Not Selected
